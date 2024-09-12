47435e5b0a3c199ca9058a38d95a915b03ce2e89
nanapatinha <117641525+nanapatinha@users.noreply.github.com>
2024-09-12 07:40:37 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ function respostaSelecionada(opcaoSelecionada){
 }
 
 function mostraResultado(){
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = `Em 2049, &{nome}`
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
     caixaResultado.classList.add("mostrar");
@@ -54,4 +54,9 @@ fuction jogarnovamente(){
     mostrapergunta();
 }
 
+function substituiNome() {
+    for(const pergunta of perguntas ) {
+        perguntas.enunciado = pergunta.enunciado.replace(/você/g,nome);
+    }
+}
 mostraPergunta();
